dd3d08617461e274256e5c5b34de7dfc861eb0ac
Dave Horton <daveh@beachdognet.com>
2020-02-15 19:24:32 -0500

    change to env vars

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const assert = require('assert');
+assert.ok(process.env.JAMBONES_OUTBOUND_ROUTE, 'missing JAMBONES_OUTBOUND_ROUTE env var');
+assert.ok(process.env.JAMBONES_INBOUND_ROUTE, 'missing JAMBONES_INBOUND_ROUTE env var');
+assert.ok(process.env.JAMBONES_NETWORK_CIDR, 'missing JAMBONES_NETWORK_CIDR env var');
+
 const express = require('express');
 const app = express();
-const config = require('config');
 const router = require('./lib/router');
-const logger = app.locals.logger = require('pino')({level: 'info'});
+const opts = Object.assign({
+  timestamp: () => {return `, "time": "${new Date().toISOString()}"`;}
+}, {level: process.env.JAMBONES_LOGLEVEL || 'info'});
+const logger = require('pino')(opts);
 
-const server = app.listen(config.get('server'), () => {
+const port = process.HTTP_PORT || 3000;
+const server = app.listen(port, () => {
   logger.info(`sbc call router app listening on ${JSON.stringify(server.address())} for http requests`);
 });
 
