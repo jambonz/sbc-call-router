740837ae040201f3128ee5264a95c2e59fdf8800
Anton Voylenko <anton.voylenko@gmail.com>
2025-01-08 20:58:22 +0200

    update deps and chore

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const assert = require('assert');
 assert.ok(process.env.K8S || process.env.JAMBONES_NETWORK_CIDR, 'missing JAMBONES_NETWORK_CIDR env var');
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.text());
 const router = require('./lib/router');
+
+const app = express();
+app.use(express.text());
+
 const opts = Object.assign({
   timestamp: () => {return `, "time": "${new Date().toISOString()}"`;}
 }, {level: process.env.JAMBONES_LOGLEVEL || 'info'});
+
 const logger = app.locals.logger = require('pino')(opts);
 
 const port = process.env.HTTP_PORT || 3000;
